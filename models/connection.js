25e517bddc52ca3765a171f5f12a619d7a3a4d9b
john doe <johndoe@gmail.com>
2021-02-22 11:46:02 +0530

    env varaibles configured

--- models/connection.js
+++ models/connection.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const mysql = require('mysql');
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'blogdb'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 
